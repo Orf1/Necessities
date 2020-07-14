205091fe1b55aeae1f9b9f900fab38c9896adf19
Orf1 <orfeas126@gmail.com>
2020-07-14 00:53:30 -0700

    Update Class1.cs

--- Necessities/Class1.cs
+++ Necessities/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Oxide.Core.Libraries.Covalence;
 
 namespace Oxide.Plugins
@@ -7,13 +7,15 @@ namespace Oxide.Plugins
     [Description("Provides many essential commands for server admins.")]
     class Necessities : CovalencePlugin
     {
+        List<IPlayer> bypassing = new List<IPlayer>();
+        
         private void Init()
         {
-            String version = "1.0.0";
+            var version = "1.0.0";
+            
             permission.RegisterPermission("necessities.admin", this);
             permission.RegisterPermission("necessities.ping", this);
             Puts("Necessities loaded. Version: " + version);
-            
         }
 
         [Command("ping"), Permission("necessities.ping")]
@@ -44,13 +46,22 @@ namespace Oxide.Plugins
                 IPlayer target = players.FindPlayer(args[0]);
                 if (target != null)
                 {
-                    if (args.Length > 1)
+                    if (!target.IsBanned)
                     {
-                        target.Ban(args[1]);
+                        if (args.Length > 1)
+                        {
+                            target.Ban(args[1]);
+                            player.Message("Banned player " + target.Name + "from the server for reason: " + args[1]);
+                        }
+                        else
+                        {
+                            target.Ban("You are banned from the server!");
+                            player.Message("Banned player " + target.Name + "from the server with no reason specified.");
+                        }
                     }
                     else
                     {
-                        target.Ban("You are banned from the server!");
+                        player.Message("Player " + target.Name + " is already banned from the server.");
                     }
                 }
                 else
@@ -61,6 +72,23 @@ namespace Oxide.Plugins
             else
             {
                 player.Message("Invalid usage! /ban [player] [time] (reason)");
+            }
+        }
+        [Command("bypass"), Permission("necessities.bypass")]
+        private void BypassCommand(IPlayer IPlayer, string command, string[] args)
+        {
+            BasePlayer player = (BasePlayer) IPlayer;
+            if (!bypassing.Contains(IPlayer))
+            {
+                AntiHack.ShouldIgnore(player);
+                bypassing.Add(IPlayer);
+                IPlayer.Message("You are now bypassing anticheat!");
+            }
+            else
+            {
+                bypassing.Remove(IPlayer);
+                //Add reset anti hack here. TODO
+                IPlayer.Message("You are no longer bypassing anticheat!");
             }
         }
     }
