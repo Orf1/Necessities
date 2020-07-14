a3baa2dbaf9c7aa2ccc2c2e91e034495c7f76a16
Orf1 <orfeas126@gmail.com>
2020-07-14 00:22:25 -0700

    Update Class1.cs

--- Necessities/Class1.cs
+++ Necessities/Class1.cs
@@ -39,12 +39,28 @@ namespace Oxide.Plugins
         [Command("ban"), Permission("necessities.ban")]
         private void BanCommand(IPlayer player, string command, string[] args)
         {
-            if (args.Length == 2)
+            if (args.Length != 0)
             {
+                IPlayer target = players.FindPlayer(args[0]);
+                if (target != null)
+                {
+                    if (args.Length > 1)
+                    {
+                        target.Ban(args[1]);
+                    }
+                    else
+                    {
+                        target.Ban("You are banned from the server!");
+                    }
+                }
+                else
+                {
+                    player.Message("Player " + args[0] + " not found.");
+                }
             }
             else
             {
-                player.Message("Invalid usage! /ban [player] [time] [reason]");
+                player.Message("Invalid usage! /ban [player] [time] (reason)");
             }
         }
     }
